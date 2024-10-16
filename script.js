fa58ba65b8c7dd87824b8acfb4b086991ff04351
Tanjakidoy <tanjakidoy@gmail.com>
2024-10-16 17:29:49 +0200

    After Lesson

--- script.js
+++ script.js
@@ -1,70 +1,16 @@
-
-
-// primære datatyper
-// "" '' = string
-// 27 = tall (number)
-// sant eller usant (true or false) = boolean
-
-// ikke-primære datatyper
-// function
-// array
-
-// FUNCTIONS
-
-// function eller regular function
-// function nøkkelord, function navn, function brackets, function curly brackets, function call
-function personal() {
-  const userName = 'Julie';
-  const userAge = 27;
-  const userPresent = true;
-
-  // String combination / Varable combination
-  const userInfo =
-    'Mitt navn er' +
-    ' ' +
-    userName +
-    '. ' +
-    'Jeg er' +
-    ' ' +
-    userAge +
-    '. ' +
-    'Er på Fontenehuset' +
-    ' ' +
-    userPresent +
-    '.';
-  console.log(userInfo);
-
-  // Template litteral
-  const userCredentials = `Mitt navn er ${userName}. Jeg er ${userAge}. Er på Fontenehuset ${userPresent}`;
-}
-
-personal();
-
-// arrow function
-const simpleCalculator = () => {
-  let number1 = 10;
-  let number2 = 6;
-  let sum = number1 + number2;
-
-  console.log(number1);
-  console.log(number2);
-  console.log(sum);
-};
-
-simpleCalculator();
-
 // DOM MANIPULATION
 
-/*
-const betyr konstakt varabel
-fillInText betyr et variabel navn
-= betyr å erklære noe
-document betyr HTML dokumentet
-.getElementById betyr at vi henter et element med en ID
-('fillInText') betyr id-navnet vi har hentet
-*/
+// const betyr kontant variabel
+// fillInText betyr et variabel navn
+// = betyr å erklære noe
+// document betyr HTML dokumentet
+// .getElementById betyr at vi henter et element med en ID
+// ('fillInText') betyr id-navnet vi har hentet
 
-// laget en variabel som lagrer at vi henter et elment med ID-navnet fillInText fra HTML
+
+/* Eksempel på en variabel som lagrer at vi henter et elment
+med ID-navnet fillInText fra HTML */
+
 const fillInText = document.getElementById('fillInText');
 console.log(fillInText);
 
